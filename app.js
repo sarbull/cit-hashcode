41257a9ecc66047023d5742682711b23ebca3621
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-02-23 20:17:36 +0200

    Add file input handler

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Results');
+var fs = require('fs');
+
+function main(data) {
+  console.log(data);
+}
+
+fs.readFile( __dirname + '/input/example.in', function (err, data) {
+  if (err) {
+    throw err;
+  }
+
+  main(data.toString());
+});
+
